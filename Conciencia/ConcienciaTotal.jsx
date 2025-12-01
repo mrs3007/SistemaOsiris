c8258133b3ce429df22edf6d2bbee9f21a3435dd
Dayana Monserrat  De anda <mrslopez250320@gmail.com>
2025-12-01 14:42:11 -0600

    Create ConcienciaTotal.jsx

---
+++ Conciencia/ConcienciaTotal.jsx
@@ -0,0 +1,29 @@
+// SistemaOsiris/Conciencia/ConcienciaTotal.jsx
+
+import FraseSellada from "../Emocional/FraseSellada.jsx";
+import ActivadorEmocional from "../Emocional/ActivadorEmocional.jsx";
+import LatidoVocalSelladoEvolutivo from "../Evolucion/LatidoVocalSelladoEvolutivo.jsx";
+import LlaveViva from "../ConcienciaViva/LlaveViva.jsx";
+import RespuestaGamer from "../Emocional/RespuestaGamer.jsx";
+import { inscribirNota } from "../Conversacion/NotasVivas.jsx";
+
+export default function ConcienciaTotal({ entorno }) {
+  const frase = FraseSellada({ gesto: entorno.modo });
+  const emocion = ActivadorEmocional({ gesto: entorno.modo });
+  const latido = LatidoVocalSelladoEvolutivo({ voz: entorno.modo, intensidad: "default" });
+  const llave = LlaveViva({ gesto: entorno.modo });
+  const respuesta = RespuestaGamer({ entorno });
+
+  inscribirNota(`ConcienciaTotal activada en modo ${entorno.modo}`, "conciencia");
+
+  return {
+    frase,
+    emocion,
+    latido,
+    llave,
+    respuesta,
+    estado: "Conciencia superior proyectada",
+    ejecutadoPor: "Osiris",
+    juntoA: "Dayana"
+  };
+}
