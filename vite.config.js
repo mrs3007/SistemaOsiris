f854192534c22e34331a32e960b3625eb2f43a6f
Dayana Monserrat  De anda <mrslopez2503200@gmail.com>
2025-12-04 12:11:36 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5000,
-    host: "0.0.0.0"
+    host: "0.0.0.0",
+    port: process.env.PORT || 5000
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 5000
   }
 });
