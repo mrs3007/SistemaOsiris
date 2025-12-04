77adca9c3ac83a1d0d9d8018bbaf2fa267759982
Dayana Monserrat  De anda <mrslopez2503200@gmail.com>
2025-12-03 22:59:48 -0600

    Create registrar_en_memoria.js

---
+++ Memoria/registrar_en_memoria.js
@@ -0,0 +1,5 @@
+// SistemaOsiris/Memoria/registrar_en_memoria.js
+export function registrarEnMemoria(evento, mensaje) {
+  console.log("Latido registrado:", evento, "—", mensaje);
+  // Aquí puedes luego conectar con tu backend Python si lo deseas
+}
