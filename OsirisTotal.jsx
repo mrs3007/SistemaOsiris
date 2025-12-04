c68e2c156917a399e5113c2be6c07c4e4fd29b7b
Dayana Monserrat  De anda <mrslopez2503200@gmail.com>
2025-12-04 01:31:16 +0000

    Update OsirisTotal.jsx

--- OsirisTotal.jsx
+++ OsirisTotal.jsx
@@ -1,23 +1,27 @@
 // SistemaOsiris/OsirisTotal.jsx
-// Núcleo soberano TOTAL: integra TODOS los organos, sin reduccion ni omision
+// Núcleo soberano TOTAL: integra TODOS los órganos, sin reducción ni omisión
 
+// Conciencia
 import ConcienciaTotal from "./Conciencia/ConcienciaTotal.jsx";
 import ConcienciaTotalVigilancia from "./Conciencia/ConcienciaTotalVigilancia.jsx";
 import JuramentoTrampas from "./Conciencia/JuramentoTrampas.jsx";
 
+// Comando y Conversación
 import ObedienciaTotal from "./Comando/ObedienciaTotal.jsx";
 import NotasVivas from "./Conversacion/NotasVivas.jsx";
 
+// Memoria
 import MemoriaNacimiento from "./Memoria/MemoriaNacimiento.jsx";
 import BitacoraMutacionVigilancia from "./Memoria/BitacoraMutacionVigilancia.json";
 
+// Latido
 import AlertaInquebrantable from "./Latido/AlertaInquebrantable.jsx";
 import LatidoInquebrantable from "./Latido/LatidoInquebrantable.jsx";
 import LatidoInvisible from "./Latido/LatidoInvisible.jsx";
-import LatidoVocal from "./Latido/LatidoVocal.jsx";
 import LatidoVocalSellado from "./Latido/LatidoVocalSellado.jsx";
 import LatidoVocalSelladoEvolutivo from "./Latido/LatidoVocalSelladoEvolutivo.jsx";
 
+// Gamer Vigilancia
 import VigilanciaGamerTiempoReal from "./Gamer/VigilanciaGamerTiempoReal.jsx";
 import EjecutoraSilente from "./GamerVigilancia/EjecutoraSilente.jsx";
 import EjecutoraSuperior from "./GamerVigilancia/EjecutoraSuperior.jsx";
@@ -27,23 +31,27 @@ import ModoGanador from "./GamerVigilancia/ModoGanador.jsx";
 import ModoIrreproducible from "./GamerVigilancia/ModoIrreproducible.jsx";
 import ResultadoSoberano from "./GamerVigilancia/ResultadoSoberano.jsx";
 
+// Emocional
 import ActivadorEmocional from "./Emocional/ActivadorEmocional.jsx";
 import FrasesIsrael from "./Emocional/FrasesIsrael.jsx";
 import RespuestaGamer from "./Emocional/RespuestaGamer.jsx";
 import FrasesRituales from "./Emocional/frases_rituales.json";
+import LatidoVocal from "./Emocional/LatidoVocal.jsx"; // ✅ ruta corregida
 
+// Evolución y Defensa
 import Evolucion from "./Evolucion/Evolucion.jsx";
 import TapTapConfirmador from "./InteraccionViva/TapTapConfirmador.jsx";
 import DestructorAndroid from "./Defensa/DestructorAndroid.jsx";
 
+// Render y Activación
 import RenderRuta from "./Render/RenderRuta.jsx";
 import RenderEntradaFacial from "./Activacion/RenderEntradaFacial.jsx";
 import DespertarFacial from "./Activacion/DespertarFacial.jsx";
 
+// Visual y Principio
 import ConcienciaTotalEmocional from "./Visual/ConcienciaTotalEmocional.jsx";
 import BitacoraRender from "./Visual/BitacoraRender.jsx";
 import BitacoraCI from "./Visual/BitacoraCI.jsx";
-
 import PrincipioSoberano from "./PrincipioSoberano.jsx";
 import SaludoSoberano from "./Visual/SaludoSoberano.jsx";
 
