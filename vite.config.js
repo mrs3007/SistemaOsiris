1684b4308103898e4d972ceddfb7da99abe43c1f
Dayana Monserrat  De anda <mrslopez2503200@gmail.com>
2025-12-05 01:19:21 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // asegura rutas correctas en Render
+  base: "/", // asegura rutas de assets en Render
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 5000
-  },
-  preview: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 5000,
-    allowedHosts: ["sistemaosiris-4m3y.onrender.com"]
-  }
+  server: { host: "0.0.0.0", port: process.env.PORT || 5000 },
+  preview: { host: "0.0.0.0", port: process.env.PORT || 5000 }
 });
