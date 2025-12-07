38e5905c543e3c7e780f1f39b8b45b5ba2495092
Dayana Monserrat  De anda <mrslopez2503200@gmail.com>
2025-12-07 08:16:05 -0600

    Actualizar vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   base: "/", // asegura rutas correctas en producción
   plugins: [react()],
   esbuild: {
-    charset: "utf8" // 🔑 fuerza UTF-8 para acentos y caracteres especiales
+    charset: "utf8" // fuerza UTF-8 para acentos y caracteres especiales
   },
   server: {
     host: "0.0.0.0",
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   preview: {
     host: "0.0.0.0",
-    port: process.env.PORT, // no lo fijes en 5000, usa el que asigna la plataforma
+    port: process.env.PORT,
     allowedHosts: ["sistemaosiris-4m3y.onrender.com"]
   }
 });
