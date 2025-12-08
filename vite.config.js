2a55e135c1905ace6ae1532c418a9764b7ede342
Dayana Monserrat  De anda <mrslopez2503200@gmail.com>
2025-12-08 15:53:09 -0600

    Actualizar vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/", // asegura rutas correctas en producción
   plugins: [react()],
-  esbuild: {
-    charset: "utf8" // fuerza UTF-8 para acentos y caracteres especiales
-  },
-  server: {
-    host: "0.0.0.0",        // Render exige escuchar en todas las interfaces
-    port: process.env.PORT  // Render asigna dinámicamente el puerto
-  },
-  preview: {
-    host: "0.0.0.0",
-    port: process.env.PORT
+  build: {
+    outDir: 'dist'
   }
-});
-
+})
