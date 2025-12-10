eba86e342a73400638c32be9cf0f9a3d556c13e2
Dayana Monserrat  De anda <mrslopez2503200@gmail.com>
2025-12-10 14:33:00 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ const isCI = process.env.CI === 'true'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: parseInt(process.env.PORT) || 5000,
+    host: '0.0.0.0'
+  },
   build: {
     outDir: isCI ? 'dist-ci' : 'dist',
     sourcemap: isCI,
