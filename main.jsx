d6c54d886c55661066e166e74fe1edfab83c8e13
Dayana Monserrat  De anda <mrslopez2503200@gmail.com>
2025-12-10 14:23:26 -0600

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,4 @@
 // main.jsx
-// Puente inicial: importa index.jsx y asegura inicializacion
 import "./index.jsx";
 
-// Inicializacion soberana de Osiris
 console.log("Sistema Osiris inicializado desde main.jsx");
