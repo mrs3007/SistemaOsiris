306aacff807470aca46e791e0ce115d2a1503c74
Dayana Monserrat  De anda <mrslopez2503200@gmail.com>
2025-12-10 14:30:01 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Detecta entorno: 'production' o 'ci'
 const isCI = process.env.CI === 'true'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: isCI ? 'dist-ci' : 'dist',
-    sourcemap: isCI, // genera mapas solo en CI/CD
+    sourcemap: isCI,
   },
   define: {
     __CI__: JSON.stringify(isCI),
+  },
+})
